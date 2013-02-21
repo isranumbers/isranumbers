d11803b67500458148c7f09364d6ef8243765d63
Israel Numbers <isranumbers@gmail.com>
2013-02-21 23:48:40 +0200

    csv_creator

---
+++ data lamas/lamas/csv_creator.py
@@ -0,0 +1,52 @@
+import xml.etree.ElementTree as ET
+import csv
+
+#creating the csv and the csv header
+with open('parameters.csv', 'wb') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
+    spamwriter.writerow(['number', 'units', 'description', 'tags', 'source', 'year', 'month', 'day'])
+
+#adding data from xml
+
+    tree = ET.parse('timeseries_e1.xml')
+    root = tree.getroot()
+
+    for child in root[0]:
+      source = 'lamas'
+      year = '-1'
+      month = '-1'
+      day = '-1'
+      description = child.attrib['topic_name'] + ', ' + child.attrib['series_name']
+      tags = ''
+      for attrib in child.attrib:
+        tags = tags + attrib + ':' + child.attrib[attrib] + ' '
+      if 'unit' in child.attrib:
+        unit = child.attrib['unit']
+      else:
+        if 'calc_method' not in child.attrib:
+          topic_name = child.attrib['topic_name']
+          if topic_name.endswith('=100'):
+            unit='Index'
+          else:
+            # skip
+            unit = 'none'
+        else:
+          calc_meth = child.attrib['calc_method']
+          if calc_meth.startswith('Index'):
+            unit = 'Index'
+          else:
+            #skip
+            unit = 'none'
+            
+      for entry in child:
+        if 'time_period' in entry.attrib:
+          time = entry.attrib['time_period']
+          if time.find('-') != -1:
+            year = time.split('-')[0]
+            month = time.split('-')[1]
+          else :
+            year = time
+        number = entry.attrib['value']
+        spamwriter.writerow([number, unit, description, tags, source,year,month,day])
+          
