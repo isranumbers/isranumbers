d98f85ee83dbca6721aa37588cc087b977fd93cc
Uri Barenholz <uri.b.mail@gmail.com>
2013-03-21 22:50:44 +0200

    updated manipulation scripts to accept non-standard characters in timeseries_e.xml

--- data lamas/lamas/csv_creator.py
+++ data lamas/lamas/csv_creator.py
@@ -1,57 +1,62 @@
-import xml.etree.ElementTree as ET
+import xml.etree.cElementTree as ET
 import csv
 import shutil
+import os
 
 #copying the original file to deal with exceptions in uploading the data
-source_name = 'timeseries_e_manipulated'
-target_name = source_name + '_leftovers.xml'
-shutil.copyfile(source_name + '.xml', target_name)
+source_name_prefix = 'timeseries_e_chunk'
+target_name_prefix = 'csv_e_chunk'
+file_index = 0
 
-#creating the csv and the csv header
-with open('parameters.csv', 'wb') as csvfile:    
-    spamwriter = csv.writer(csvfile, delimiter=',',
-                            quotechar='"', quoting=csv.QUOTE_ALL)
-    spamwriter.writerow(['number', 'units', 'description', 'tags', 'source', 'year', 'month', 'day'])
+while os.path.exists(source_name_prefix + '%d.xml' % file_index):
+    source_name = source_name_prefix + '%d.xml' % file_index
+    print ('opening: ' + source_name)
+#create the csv and the csv header
+    with open(target_name_prefix + '%d.csv' % file_index, 'wb') as csvfile:    
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_ALL)
+        spamwriter.writerow(['number', 'units', 'description', 'tags', 'source', 'year', 'month', 'day'])
 
 #adding data from xml
 
-    tree = ET.parse(target_name)
-    root = tree.getroot()
-    for child in root[0]:
-      source = 'lamas'
-      year = '-1'
-      month = '-1'
-      day = '-1'
-      description = child.attrib['topic_name'] + ', ' + child.attrib['series_name']
-      tags = ''
-      for attrib in child.attrib:
-        tags = tags + attrib + ':' + child.attrib[attrib] + ' '
-      if 'unit' in child.attrib:
-        unit = child.attrib['unit']
-      else:
-        if 'calc_method' not in child.attrib:
-          topic_name = child.attrib['topic_name']
-          if topic_name.endswith('=100'):
-            unit='Index'
+        tree = ET.parse(source_name)
+        root = tree.getroot()
+        for child in root[0]:
+          source = 'lamas'
+          year = '-1'
+          month = '-1'
+          day = '-1'
+          description = child.attrib['topic_name'] + ', ' + child.attrib['series_name']
+          tags = ''
+          for attrib in child.attrib:
+            tags = tags + attrib + ':' + child.attrib[attrib] + ' '
+          if 'unit' in child.attrib:
+            unit = child.attrib['unit']
           else:
-            # skip
-            unit = 'none'
-        else:
-          calc_meth = child.attrib['calc_method']
-          if calc_meth.startswith('Index'):
-            unit = 'Index'
-          else:
-            #skip
-            unit = 'none'
-            
-      for entry in child:
-        if 'time_period' in entry.attrib:
-          time = entry.attrib['time_period']
-          if time.find('-') != -1:
-            year = time.split('-')[0]
-            month = time.split('-')[1]
-          else :
-            year = time
-        number = entry.attrib['value']
-        spamwriter.writerow([number, unit, description, tags, source,year,month,day])
-          
+            if 'calc_method' not in child.attrib:
+              topic_name = child.attrib['topic_name']
+              if topic_name.endswith('=100'):
+                unit='Index'
+              else:
+                # skip
+                unit = 'none'
+            else:
+              calc_meth = child.attrib['calc_method']
+              if calc_meth.startswith('Index'):
+                unit = 'Index'
+              else:
+                #skip
+                unit = 'none'
+                
+          for entry in child:
+            if 'time_period' in entry.attrib:
+              time = entry.attrib['time_period']
+              if time.find('-') != -1:
+                year = time.split('-')[0]
+                month = time.split('-')[1]
+              else :
+                year = time
+            number = entry.attrib['value']
+            spamwriter.writerow([number, unit, description, tags, source,year,month,day])
+    file_index+=1
+
--- data lamas/lamas/manipulate_xml.py
+++ data lamas/lamas/manipulate_xml.py
@@ -1,11 +1,76 @@
 #manipulate original data to be parsible
 import shutil
 
-source_name = 'timeseries_e'
-target_name = source_name + '_manipulated.xml'
+source_name = './timeseries_e'
+target_name_prefix = source_name + '_chunk'
 
 source = open(source_name + '.xml', 'r')
-target = open(target_name, 'w')
-target.write(source.readline().replace('8859-8-i','8859-8'))
+fileindex = 0
+#get init:
+filestart = []
+suffix = []
 for line in source:
-  target.write(line.replace('&', '&amp;amp;'))
+    if line.startswith('<Series'):
+        firstline = line
+        break
+    else:
+        line = line.replace('iso-8859-8-i','utf-8')
+        filestart.append(line)
+
+targetname = target_name_prefix + '%d.xml' % fileindex
+target = []
+target.append(open(targetname,'w'))
+
+for line in filestart:
+    target[fileindex].write(line)
+target[fileindex].write(firstline)
+count = 0
+for line in source:
+    if line.startswith('</Data'):
+        suffix.append(line)
+        break
+    line = line.replace('& ', '&amp; ')
+    line = line.replace('&glass', '&amp;glass')
+    line = line.replace('&artic', '&amp;artic')
+    line = line.replace('&scien', '&amp;scien')
+    line = line.replace('&Samaria', '&amp;Samaria')
+    line = line.replace(chr(0xa0), ' ')
+    line = line.replace(chr(0xb7), ' ')
+    line = line.replace(chr(0xee), '')
+    line = line.replace(chr(0x96), ' ')
+    line = line.replace(chr(0x92), "'")
+
+    target[fileindex].write(line)
+    count+=1
+    if count == 10000:
+        for line1 in source:
+            line1 = line1.replace('& ', '&amp; ')
+            line1 = line1.replace('&glass', '&amp;glass')
+            line1 = line1.replace('&artic', '&amp;artic')
+            line1 = line1.replace('&scien', '&amp;scien')
+            line1 = line1.replace(chr(0xa0), ' ')
+            line1 = line1.replace(chr(0xb7), ' ')
+            line1 = line1.replace('&Samaria', '&amp;Samaria')
+            line1 = line1.replace(chr(0xee), '')
+            line1 = line1.replace(chr(0x96), ' ')
+            line1 = line1.replace(chr(0x92), "'")
+            if line1.startswith('<Series'):
+                nextline = line1
+                fileindex+=1
+                targetname = target_name_prefix + '%d.xml' % fileindex
+                target.append(open(targetname,'w'))
+                for line2 in filestart:
+                    target[fileindex].write(line2)
+                count = 0
+                target[fileindex].write(line1)
+                break
+            else:
+                target[fileindex].write(line1)
+
+for line in source:
+    suffix.append(line)
+for targetf in target:
+    for line in suffix:
+        targetf.write(line)
+# for line in source:
+  # target.write(line.replace('\a0', ' '))
